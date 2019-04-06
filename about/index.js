04d0a046f96a45f1436760b9ba934ae7c68d2eac
Luka Bratos <luka@pusher.com>
2019-04-06 18:59:49 +0100

    Use res.status(status).send(body)

--- about/index.js
+++ about/index.js
@@ -8,13 +8,10 @@ app.use(helmet());
 
 app.get("*", (req, res) => {
   res.set("Content-Type", "text/html");
-  res.send(
-    200,
-    `
-        <h1>Hello from Express path '/about' on Now 2.0!</h1>
-        <h2>Go to <a href="/">/</a></h2>
-    `
-  );
+  res.status(200).send(`
+    <h1>Hello from Express path '/about' on Now 2.0!</h1>
+    <h2>Go to <a href="/">/</a></h2>
+`);
 });
 
 module.exports = app;
--- index.js
+++ index.js
@@ -8,13 +8,10 @@ app.use(helmet());
 
 app.get("*", (req, res) => {
   res.set("Content-Type", "text/html");
-  res.send(
-    200,
-    `
-        <h1>Hello from Express path '/' on Now 2.0!</h1>
-        <h2>Go to <a href="/about">/about</a></h2>
-    `
-  );
+  res.status(200).send(`
+    <h1>Hello from Express path '/' on Now 2.0!</h1>
+    <h2>Go to <a href="/about">/about</a></h2>
+`);
 });
 
 module.exports = app;
