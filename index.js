6c4981db0f54a4b921c7a9918bab7affcc3340a8
Luka Bratos <luka@pusher.com>
2019-04-07 11:23:34 +0100

    Use res.status(status).send(body)

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ app.use(helmet());
 
 app.get("*", (req, res) => {
   res.set("Content-Type", "text/html");
-  res.send(
-    200,
-    `
+  res.status(200).send(`
   <h1>API List:</h1>
   <h2><a href="/api/val202">Val 202</a></h2>
   <h2><a href="/api/ra1">Radio Prvi</a></h2>
   <h2><a href="/api/rmb">Radio Maribor</a></h2>
-`
-  );
+`);
 });
 
 module.exports = app;
