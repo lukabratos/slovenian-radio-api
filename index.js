00b64c371f28e8892707e6c2534f1dda5d5978d6
Luka Bratos <luka@pusher.com>
2019-04-07 11:19:47 +0100

    Update index.js with API list

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require("express");
 const helmet = require("helmet");
 
 const app = express();
-
-// add some security-related headers to the response
 app.use(helmet());
 
-app.get("/", (req, res) => {
-  res.set("Content-Type", "application/json");
-  res.status(200).send(JSON.stringify({ status: "OK" }));
+app.get("*", (req, res) => {
+  res.set("Content-Type", "text/html");
+  res.send(
+    200,
+    `
+  <h1>API List:</h1>
+  <h2><a href="/api/val202">Val 202</a></h2>
+  <h2><a href="/api/ra1">Radio Prvi</a></h2>
+  <h2><a href="/api/rmb">Radio Maribor</a></h2>
+`
+  );
 });
 
 module.exports = app;
