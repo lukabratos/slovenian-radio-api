0ec260e733e3dc3cadbb41596c6a25d5a21f1cee
Luka Bratos <luka@pusher.com>
2019-04-07 10:39:04 +0100

    Tidy up code for Radio Val 202

--- api/val202.js
+++ api/val202.js
@@ -4,50 +4,62 @@ const request = require("request");
 const cheerio = require("cheerio");
 
 const app = express();
-
-// add some security-related headers to the response
 app.use(helmet());
+
+function parseScheduleTime($) {
+  var scheduleTimeList = [];
+  const scheduleTime = $("div.schedule-time");
+
+  scheduleTime.each(function() {
+    scheduleTimeList.push(
+      $(this)
+        .text()
+        .trim()
+    );
+  });
+
+  return scheduleTimeList;
+}
+
+function parseScheduleTitle($) {
+  var scheduleTitleList = [];
+  const scheduleTitle = $("div.schedule-title");
+
+  scheduleTitle.each(function() {
+    scheduleTitleList.push(
+      $(this)
+        .text()
+        .trim()
+    );
+  });
+
+  return scheduleTitleList;
+}
+
+function generateScheduleJSON(timeList, titleList) {
+  var result = [];
+  for (i = 0; i < timeList.length; i++) {
+    result.push({ time: timeList[i], title: titleList[i] });
+  }
+
+  return result;
+}
 
 app.get("/api/val202", (req, res) => {
   res.set("Content-Type", "application/json");
 
   request("https://val202.rtvslo.si/spored/", function(error, response, body) {
-    if (error != null) {
+    if (error != null && response.statusCode != 200) {
       console.error("error:", error);
     }
 
-    var result = [];
-    var scheduleTimeList = [];
-    var scheduleTitleList = [];
-
     const $ = cheerio.load(body);
-    const scheduleTime = $("div.schedule-time");
-    const scheduleTitle = $("div.schedule-title");
+    var scheduleTimeList = parseScheduleTime($);
+    var scheduleTitleList = parseScheduleTitle($);
 
-    scheduleTime.each(function(i, e) {
-      scheduleTimeList.push(
-        $(this)
-          .text()
-          .trim()
-      );
-    });
-
-    scheduleTitle.each(function(i, e) {
-      scheduleTitleList.push(
-        $(this)
-          .text()
-          .trim()
-      );
-    });
-
-    for (i = 0; i < scheduleTitleList.length; i++) {
-      result.push({ time: scheduleTimeList[i], title: scheduleTitleList[i] });
-    }
-
+    const result = generateScheduleJSON(scheduleTimeList, scheduleTitleList);
     res.status(200).send(JSON.stringify(result));
   });
 });
-
-app.listen(3000);
 
 module.exports = app;
